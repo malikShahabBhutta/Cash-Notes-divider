93faf53639379d29030b2e3f70295a0ad7c3cfa5
malikShahabBhutta <73873661+malikShahabBhutta@users.noreply.github.com>
2022-12-20 01:59:18 +0500

    LogicPartOfThisProject
    main working of project

--- javascript.js
+++ javascript.js
@@ -2,12 +2,15 @@ const billamount=document.getElementById("bill-amount");
 const cashgiven=document.getElementById("cash-given");
 const checkButton=document.getElementById("clickbutton");
 const message=document.getElementById("error-message");
+const noOfNotes=document.querySelectorAll(".no-of-notes");//(".no-of-notes");
+
+const availableNotes=[5000,1000,500,100,50,20,10,5,2,1]
 checkButton.addEventListener("click",function validateBillAndCashAmount(){
         message.style.display="none";
         if(billamount.value>0){
                 if(cashgiven.value>=billamount.value){
                     const amountToBeReturn = cashgiven.value-billamount.value
-                    calculaterchange(amountToBeReturn);
+                    calculaterChange(amountToBeReturn);
 
                 }else{
                     showmessage("the cash provided shoul atleast be equle to the bill amount");
@@ -17,7 +20,12 @@ checkButton.addEventListener("click",function validateBillAndCashAmount(){
             showmessage("Invalid amount");
         }
 })
-    function calculaterchange(){
+    function calculaterChange(amountToBeReturn){
+        for(i=0;i<availableNotes.length;i++){
+            const numberOfNotes = Math.trunc(amountToBeReturn/availableNotes[i]);
+        }
+            amountToBeReturn %= availableNotes[i];
+            noOfNotes[i].innerHTML=numberOfNotes;
 
     } 
         function showmessage(msg){
