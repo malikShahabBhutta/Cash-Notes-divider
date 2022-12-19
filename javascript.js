b9153907fa7eb6838833a7a19b8df39bcab5040c
malikShahabBhutta <73873661+malikShahabBhutta@users.noreply.github.com>
2022-12-20 01:07:09 +0500

    showErrorByFunction
    in this we make function if user input wrong amount then it show error

--- javascript.js
+++ javascript.js
@@ -3,12 +3,26 @@ const cashgiven=document.getElementById("cash-given");
 const checkButton=document.getElementById("clickbutton");
 const message=document.getElementById("error-message");
 checkButton.addEventListener("click",function validateBillAndCashAmount(){
+        message.style.display="none";
+        if(billamount.value>0){
+                if(cashgiven.value>=billamount.value){
+                    const amountToBeReturn = cashgiven.value-billamount.value
+                    calculaterchange(amountToBeReturn);
 
-        if(billamount>0){
-
-        }else{
-            message.innerHTML="The BIll Amount Should Be greater Then 0";
+                }else{
+                    showmessage("the cash provided shoul atleast be equle to the bill amount");
+                }
+        }
+        else{
+            showmessage("Invalid amount");
         }
 })
+    function calculaterchange(){
+
+    } 
+        function showmessage(msg){
+    message.style.display="block";
+    message.innerHTML=msg;
+}
 
 
